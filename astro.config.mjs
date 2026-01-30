a2f390408804c263fad0c9dda45bb9ae8827e7a5
Alexandre-Guillet <alexandre.guillet@edu.univ-fcomte.fr>
2026-01-30 14:32:22 +0100

    2'eme commit TP3 Images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  adapter: netlify()
+  image: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8090",
+      },
+    ],
+  },
+
+  adapter: netlify(),
 });
