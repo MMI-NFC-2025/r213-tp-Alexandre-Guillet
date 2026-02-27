c7f9d3d82346daa337802d3b88a074c42ad1f3b4
Alexandre-Guillet <alexandre.guillet@edu.univ-fcomte.fr>
2026-02-27 14:23:22 +0100

    TP5 add offres

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -52,3 +52,19 @@ export async function byPriceMinMax(prixmin, prixmax) {
     .getFullList({ filter: `prix > ${prixmin} && prix < ${prixmax}` });
   return records;
 }
+
+export async function addOffre(house) {
+  try {
+    await db.collection("Maison").create(house);
+    return {
+      success: true,
+      message: "Offre ajoutée avec succès",
+    };
+  } catch (error) {
+    console.log("Une erreur est survenue en ajoutant la maison", error);
+    return {
+      success: false,
+      message: "Une erreur est survenue en ajoutant la maison",
+    };
+  }
+}
